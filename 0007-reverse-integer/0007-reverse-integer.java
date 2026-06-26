a3db02a528b315b62e36f13a332e24eeecc9e033
Subasri M <suba212005@gmail.com>
2026-06-26 14:33:25 +0530

    Fix boundary condition for integer reversal

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,7 +3,7 @@ class Solution {
         int rev=0;
         while(x!=0){
            int digit=x%10;
-           if(rev>Integer.MAX_VALUE/10 || rev==Integer.MIN_VALUE/10 && digit>7){
+           if(rev>Integer.MAX_VALUE/10 || rev==Integer.MAX_VALUE/10 && digit>7){
                  return 0;
            }
            if(rev<Integer.MIN_VALUE/10 || rev==Integer.MIN_VALUE/10 && digit<-8) {
