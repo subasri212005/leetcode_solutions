3a6d68a0ef36ee4bf83f43552d7d06d1d6759d02
Subasri M <suba212005@gmail.com>
2026-06-28 03:28:20 +0530

    Time: 1 ms (97.01%), Space: 47.2 MB (35.85%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -3,12 +3,13 @@ class Solution {
        int[] numcopy=new int[nums.length*2];
        for(int i=0;i<nums.length;i++){
         numcopy[i]=nums[i];
+        numcopy[i+nums.length]=nums[i];
        }
-       int j=0;
-       for(int i=nums.length;i<numcopy.length;i++){
-        numcopy[i]=nums[j];
-        j++;
-       } 
+    //    int j=0;
+    //    for(int i=nums.length;i<numcopy.length;i++){
+    //     numcopy[i]=nums[j];
+    //     j++;
+    //    } 
        return numcopy;
     }
 }
