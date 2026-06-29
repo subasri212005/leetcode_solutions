9f2397075e372f4a0e4ec65777f86f7d36d02403
Subasri M <suba212005@gmail.com>
2026-06-29 16:24:24 +0530

    Time: 0 ms (100%), Space: 43.7 MB (8.19%) - LeetHub

---
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+        int count=0,n=patterns.length;
+        for(int i=0;i<n;i++){
+            if(word.contains(patterns[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+}
